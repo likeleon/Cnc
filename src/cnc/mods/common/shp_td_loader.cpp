ff95ef46d9577e0dcd432fef8639b11fe0548421
Sung Ho Kim <likeleon@gmail.com>
2016-01-16 09:16:22 +0900

    중간 서밋

--- src/cnc/mods/common/shp_td_loader.cpp
+++ src/cnc/mods/common/shp_td_loader.cpp
@@ -32,10 +32,41 @@ static bool IsShpTD(const std::vector<char>& s) {
   return b == 0x20 || b == 0x40 || b == 0x80;
 }
 
-bool ShpTDLoader::TryParseSprite(const std::vector<char>& s, std::vector<ISpriteFramePtr>& /*frames*/) {
+class ShpTDSprite {
+public:
+  enum class Format {
+    XORPrev = 0x20,
+    XORLCW = 0x40,
+    LCW = 0x80
+  };
+
+  using Ptr = std::shared_ptr<ShpTDSprite>;
+
+  class ImageHeader : public ISpriteFrame {
+  public:
+    ImageHeader(const std::vector<char>& stream, ShpTDSprite::Ptr reader);
+    
+    const Size & size() const override;
+    const Size& frame_size() const override;
+    const Float2& offset() const override;
+    const std::vector<char>& data() const override;
+    bool disable_export_padding() const override;
+  
+  private:
+    ShpTDSprite::Ptr reader_;
+    std::vector<char> data_;
+  };
+
+  explicit ShpTDSprite(const std::vector<char>& stream);
+};
+
+bool ShpTDLoader::TryParseSprite(const std::vector<char>& s, std::vector<ISpriteFramePtr>& frames) {
   if (!IsShpTD(s)) {
     return false;
   }
+  
+  ShpTDSprite sprite(s);
+  std::move(sprite.frames().begin(), sprite.frames().end(), std::back_inserter(frames));
   return false;
 }
 
