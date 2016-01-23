102e5d0e1129005b7f04e1c3dcba3bcd5ba6d1af
Sung Ho Kim <likeleon@gmail.com>
2016-01-23 23:39:51 +0900

    Immutable/Mutable Palette

--- src/cnc/palette.cpp
+++ src/cnc/palette.cpp
@@ -1,9 +1,73 @@
 #include "cnc/stdafx.h"
 #include "cnc/palette.h"
+#include "cnc/color.h"
+#include "cnc/file.h"
+#include "cnc/buffer_utils.h"
 
 namespace cnc {
 
-Color Palette::GetColor(IPalette&, int32_t index) {
+Color Palette::GetColor(const IPalette& palette, int32_t index) {
+  return Color(static_cast<int32_t>(palette[index]));
+}
+
+ImmutablePalette::ImmutablePalette(const std::string& filename, const std::vector<int32_t>& remap_shadow) {
+  LoadFromStream(File::ReadAllBytes(filename), remap_shadow);
+}
+
+ImmutablePalette::ImmutablePalette(const std::vector<char>& s, const std::vector<int32_t>& remap_shadow) {
+  LoadFromStream(s, remap_shadow);
+}
+
+ImmutablePalette::ImmutablePalette(const IPalette& p) {
+  for (auto i = 0; i < Palette::Size; ++i) {
+    colors_[i] = p[i];
+  }
+}
+
+void ImmutablePalette::LoadFromStream(const std::vector<char>& s, const std::vector<int32_t>& remap_shadow) {
+  uint32_t offset = 0;
+  for (auto i = 0; i < Palette::Size; ++i) {
+    auto r = static_cast<uint8_t>(BufferUtils::ReadUInt8(s, offset) << 2);
+    auto g = static_cast<uint8_t>(BufferUtils::ReadUInt8(s, offset) << 2);
+    auto b = static_cast<uint8_t>(BufferUtils::ReadUInt8(s, offset) << 2);
+    colors_[i] = static_cast<uint32_t>((255 << 24) | (r << 16) | (g << 8) | b);
+  }
+  colors_[0] = 0; // convert black background -> transparency
+  for (int32_t i : remap_shadow) {
+    colors_[i] = 140u << 24;
+  }
+}
+
+uint32_t ImmutablePalette::operator[](int32_t index) const {
+  return colors_[index];
+}
+
+void ImmutablePalette::CopyToArray(std::vector<char>& destination, int32_t destination_offset) const {
+  destination.resize(destination_offset * 4 + Palette::Size * 4);
+  memcpy(&destination[destination_offset * 4], &colors_[0], Palette::Size * 4);
+}
+
+MutablePalette::MutablePalette(const IPalette& p) {
+  SetFromPalette(p);
+}
+
+void MutablePalette::SetFromPalette(const IPalette& p) {
+  std::vector<char> buffer;
+  p.CopyToArray(buffer, 0);
+  memcpy(&colors_[0], &buffer[0], buffer.size());
+}
+
+uint32_t MutablePalette::operator[](int32_t index) const {
+  return colors_[index];
+}
+
+uint32_t& MutablePalette::operator[](int32_t index) {
+  return colors_[index];
+}
+
+void MutablePalette::CopyToArray(std::vector<char>& destination, int32_t destination_offset) const {
+  destination.resize(destination_offset * 4 + Palette::Size * 4);
+  memcpy(&destination[destination_offset * 4], &colors_[0], Palette::Size * 4);
 }
 
 }
--- src/cnc/palette.h
+++ src/cnc/palette.h
@@ -8,7 +8,7 @@ class IPalette {
 public:
   virtual ~IPalette() {}
 
-  virtual uint32_t operator()(int32_t index) const = 0;
+  virtual uint32_t operator[](int32_t index) const = 0;
   virtual void CopyToArray(std::vector<char>& destination, int32_t destination_offset) const = 0;
 };
 
@@ -16,9 +16,39 @@ class Palette {
 public:
   Palette() = delete;
 
-  static Color GetColor(IPalette&, int32_t index);
+  static Color GetColor(const IPalette& palette, int32_t index);
 
   static const int32_t Size = 256;
+};
+
+class ImmutablePalette : public IPalette {
+public:
+  ImmutablePalette(const std::string& filename, const std::vector<int32_t>& remap_shadow);
+  ImmutablePalette(const std::vector<char>& s, const std::vector<int32_t>& remap_shadow);
+  ImmutablePalette(const IPalette& p);
+
+  uint32_t operator[](int32_t index) const override;
+  void CopyToArray(std::vector<char>& destination, int32_t destination_offset) const override;
+
+private:
+  void LoadFromStream(const std::vector<char>& s, const std::vector<int32_t>& remap_shadow);
+
+  std::array<uint32_t, Palette::Size> colors_;
+};
+
+class MutablePalette : public IPalette {
+public:
+  MutablePalette(const IPalette& p);
+
+  void SetFromPalette(const IPalette& p);
+
+  uint32_t operator[](int32_t index) const override;
+  uint32_t& operator[](int32_t index);
+  
+  void CopyToArray(std::vector<char>& destination, int32_t destination_offset) const override;
+
+private:
+  std::array<uint32_t, Palette::Size> colors_;
 };
 
 }
