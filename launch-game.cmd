10a7bd7675e193f2727be54c9d260957e743caec
Sung Ho Kim <likeleon@gmail.com>
2016-02-03 01:16:18 +0900

    빌드, 게임 실행 스크립트

---
+++ launch-game.cmd
@@ -0,0 +1,3 @@
+@echo on
+
+start bin/Release/Cnc.Game.exe %*
--- make.ps1
+++ make.ps1
@@ -1,8 +1,29 @@
+function FindMSBuild
+{
+	$msBuildVersions = @("14.0")
+	foreach ($msBuildVersion in $msBuildVersions)
+	{
+		$key = "HKLM:\SOFTWARE\Microsoft\MSBuild\ToolsVersions\{0}" -f $msBuildVersion
+		$property = Get-ItemProperty $key -ErrorAction SilentlyContinue
+		if ($property -eq $null -or $property.MSBuildToolsPath -eq $null)
+		{
+			continue
+		}
+		$path = Join-Path $property.MSBuildToolsPath -ChildPath "MSBuild.exe"
+		if (Test-Path $path)
+		{
+			return $path
+		}
+	}
+	return $null
+}
+
 if ($args.Length -eq 0)
 {
 	echo "Command list:"
 	echo ""
-	echo "  all    Builds the game and its development tools."
+	echo "  all             Builds the game and its development tools."
+	echo "  dependencies    Thid third-party libraries."
 	echo ""
 	$command = (Read-Host "Enter command").Split(' ', 2)
 }
@@ -11,21 +32,59 @@ else
 	$command = $args
 }
 
-if ($command -eq "dependencies")
+if ($command -eq "all")
 {
 	$msBuild = FindMSBuild
-	$msBuildArguments = "/t:Rebuild /nr:false"
 	if ($msBuild -eq $null)
 	{
 		echo "Unable to locate an appropriate version of MSBuild."
 	}
 	else
 	{
-		$targetSolution = "src/third_party/lpng162/projects/vstudio/vstudio.sln"
-		$proc = Start-Process  $msbuild $msBuildArguments
+		$msBuildArguments = "Cnc.sln /t:Build /p:Configuration=Release /nr:false"
+		$proc = Start-Process $msbuild $msBuildArguments -NoNewWindow -PassThru -Wait
+		if ($proc.ExitCode -ne 0)
+		{
+			echo "Cnc build failed."
+		}
+		else
+		{
+			echo "Build succeeded."
+		}
 	}
-	cd src/third_party/lpng162/projects/vstudio
-	cd ../../../../../
+}
+elseif ($command -eq "dependencies")
+{
+	$msBuild = FindMSBuild
+	if ($msBuild -eq $null)
+	{
+		echo "Unable to locate an appropriate version of MSBuild."
+	}
+	else
+	{
+		$lpngDir = "src/third_party/lpng162/projects/vstudio"
+		$msBuildArguments = $lpngDir + "/vstudio.sln /t:zlib;libpng /p:Configuration=Release /nr:false"
+		$proc = Start-Process $msbuild $msBuildArguments -NoNewWindow -PassThru -Wait
+		if ($proc.ExitCode -ne 0)
+		{
+			echo "lpng162 build failed."
+		}
+		else
+		{
+			$configurations = @("Debug", "Release")
+			foreach ($configuration in $configurations)
+			{
+				$sourceFiles = $lpngDir + "/Release/*.dll"
+				$destinationFolder = "bin/" + $configuration
+				if ((Test-Path $destinationFolder) -eq 0)
+				{
+					mkdir $destinationFolder
+				}
+				cp $sourceFiles $destinationFolder
+			}
+			echo "Dependencies build done and copied."
+		}
+	}
 }
 else
 {
