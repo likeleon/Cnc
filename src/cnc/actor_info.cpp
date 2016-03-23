e037c4089ad8dc459940fa4da268bae00a0e2781
Sung Ho Kim <likeleon@gmail.com>
2016-03-23 23:50:53 +0900

    ActorInfo 누락

---
+++ src/cnc/actor_info.cpp
@@ -0,0 +1,9 @@
+#include "cnc/stdafx.h"
+#include "cnc/actor_info.h"
+
+namespace cnc {
+
+ActorInfo::ActorInfo(ObjectCreator& /*creator*/, const std::string& /*name*/, const MiniYaml& /*node*/, const std::map<std::string, MiniYaml>& /*all_units*/) {
+}
+
+}
